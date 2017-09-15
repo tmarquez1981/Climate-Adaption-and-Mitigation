2520f82398c06d2ffd98f356a0e5648c9186d0e5
Thomas Marquez <Thomas Marquez>
2017-09-14 22:57:20 -0700

    Add project files.

---
+++ Climate Adaption and Mitigation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Climate_Adaption_and_Mitigation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
